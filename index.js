2d41855ed7b1006f8e548236f8812d73cc691060
Ricky Caballero <cabaethernet@gmail.com>
2018-01-19 10:44:00 -0500

    First Commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  * Dependencies
  */
 var express = require("express");
+var serveStatic = require('serve-static');
+var path = require('path');
 var app = express();
 var port = 3700;
 
@@ -11,10 +13,9 @@ var io = require('socket.io').listen(app.listen(port));
 /**
  * Configuration
  */
-app.set('views', __dirname + '/tpl');
-app.set('view engine', "jade");
-app.engine('jade', require('jade').__express);
-
+app.set('views', path.join(__dirname + '/public/views'));
+app.set('view engine', "pug");
+app.engine('pug', require('pug').__express);
 /**
  * Routes
  */
@@ -22,7 +23,7 @@ app.get("/", function(req, res){
 	res.render("page");
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public')));
 
 io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'welcome to the chat' });
