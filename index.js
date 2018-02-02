a294e4ac894573b19bf5f9ad5a7182dfb214a5a7
Ricky Caballero <cabaethernet@gmail.com>
2018-02-02 01:49:50 -0500

    |add|User Online Add Functionality

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var serveStatic = require('serve-static');
 var path = require('path');
 var app = express();
 var port = 3700;
-
+var users = [];
+var connections = [];
 var io = require('socket.io').listen(app.listen(port));
 
 
@@ -26,15 +27,63 @@ app.get("/", function(req, res){
 });
 
 app.use(express.static(path.join(__dirname + '/public')));
+console.log("Listening on port " + port);
+
+
+/**
+* Chatroom
+*/
 
 io.sockets.on('connection', function (socket) {
+	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
+	updateUsernames();
     socket.emit('message', { message: 'Welcome! Enter a username and get started!' });
+
+//Add Users
+    socket.on('add', function (data) {
+    	
+    	socket.username = data;
+    	users.push(socket.username);
+    	updateUsernames();
+
+    	console.log('add', data);
+    	io.sockets.emit('username', data);
+
+    });
+
+//Send Messages
     socket.on('send', function (data) {
     	console.log('send', data);
         io.sockets.emit('message', data);
-    });
-});
 
-console.log("Listening on port " + port);
+    });   
+
+
+
+//Disconnect
+    socket.on('disconnect', function(data) {
+
+    	users.splice(users.indexOf(socket.username), 1);
+    	console.log('disconnect', data);
+    	updateUsernames(data);
+    	connections.splice(connections.indexOf(socket), 1);
+    	console.log('Disonnected: %s sockets connected', connections.length);
+    })
+
+//Username Update
+    function updateUsernames(data) {
+    	io.sockets.emit('get users', users);
+    }
+    console.log('made socket connection', socket.id);
+
+	});
+
+
+
+	
+
+
+
 
 
--- public/chat.js
+++ public/chat.js
@@ -1,52 +1,145 @@
 window.onload = function() {
  
-    var messages = [];
-    var socket = io.connect('http://localhost:3700');
-    var field = document.getElementById("field");
-    var sendButton = document.getElementById("send");
-    var content = document.getElementById("content");
-    var name = document.getElementById('name');
-    var friendlist = document.getElementById('friendlist');
+	var messages = [];
+	var connections = [];
+	var socket = io.connect('http://localhost:3700');
+	var field = document.getElementById("field");
+	var sendButton = document.getElementById("send");
+	var content = document.getElementById("content");
+	var name = document.getElementById('name');
+	var friendlist = document.getElementById('friendlist');
+	var login = document.getElementById('login');
+	var logout = document.getElementById('logout');
+	
+	
+	var usersonline = document.getElementById('usersonline');
+	
 
-    socket.on('message', function (data) {
-        if(data.message) {
-            messages.push(data);
-            console.log('all the messages', messages);
-            var html = '';
-            for (var i=0; i < messages.length; i++) {
-                var username = "Server";
-                if(messages[i].username) {
-                    username = messages[i].username;
-                }
+	
 
-                html += '<b>' + username + ': </b>';
-                html += messages[i].message + '<br />';
-            }
+	socket.on('message', function (data) {
+		if(data.message) {
+			messages.push(data);
+			console.log('all the messages', messages);
+			var html = '';
+			
+			for (var i=0; i < messages.length; i++) {
+				var username = "Server";
+				if(messages[i].username) {
+					username = messages[i].username;
+				}
 
-            content.innerHTML = html;
-            content.scrollTop = content.scrollHeight;
-        } else {
-            console.log("There is a problem:", data);
-        }
-    });
+				html += '<b>' + username + ': </b>';
+				html += messages[i].message + '<br />';
+				
+			}
+			
+			content.innerHTML = html;
+			content.scrollTop = content.scrollHeight;
+		} else {
+			console.log("There is a problem:", data);
+		}
+	});
+	
+	socket.on('add', function(data) {
+		addUser(data);
+	})
+
+	socket.on('get users', function(data) {
+		var html = '';
+		for(i= 0;i < data.length;i++) {
+			html += '<li id="usersonline">' + data[i] + '</li>';
+		}
+		usersonline.innerHTML = html;
+	})
+	
+	
+	 
+
+ /*
+	Click and Key Events
+*/
+	sendButton.onclick = function() {
+		var text = field.value;
+		socket.emit('send', { message: text, username: name.value  });
+
+		field.value = "";
+		console.log('hello');
+	};
+	field.onkeypress = function(event) {
+		var key = event.keyCode;
+		if (key == 13 || key.which == 13) {
+			var text = field.value;
+			socket.emit('send', { message: text, username: name.value  });
+			field.value = "";
+			console.log('its working')
+		};
+	};
+
+
+	
+
+
+
+	
+	login.onclick = function() {
+		
+		var text= name.value;
+		
+		socket.emit('add', text, function(data) {
+
+			text;
+		});
+		socket.emit('get users', function(data) {
+			text;
+		});
+		
+
+		
+	
+		$(this).prop('disabled', true);
+		$(name).prop('disabled', true);
+		$(logout).prop('disabled', false);
+		$(field).prop('disabled', false);
+		console.log('hey');
+		
+	};
+	
+	name.onkeypress = function(event) {
+		var key = event.keyCode;
+		if (key.which == 13 && key.shiftKey == false) {
+			friendlist.innerHTML += name.value;
+	
+			$(login).prop('disabled', true);
+			$(this).prop('disabled', true);
+			$(logout).prop('disabled', false);
+			$(field).prop('disabled', false);
+		};
+	};
+
+
+	logout.onclick = function() {
+		name.value = '';
+		
+		var text = name.value;
+		socket.emit('disconnect', text, function(data) {
+			text;
+		});
+		socket.emit('get users', text, function(data) {
+			text;
+		});
+		//friendlist.innerHTML = '';
+
+		$(login).prop('disabled', false);
+		$(name).prop('disabled', false);
+		$(this).prop('disabled', true);
+		$(field).prop('disabled', true);
+		
+	}
  
-    sendButton.onclick = function() {
-        var text = field.value;
-        socket.emit('send', { message: text, username: name.value  });
-        field.value = "";
-    };
-    field.onkeypress = function(event) {
-        var key = event.keyCode;
-        if (key == 13 || key.which == 13) {
-            var text = field.value;
-            socket.emit('send', { message: text, username: name.value  });
-            field.value = "";
-            console.log('its working')
-        };
-    };
 
-
- 
+ 	
 }
 
 
+	
