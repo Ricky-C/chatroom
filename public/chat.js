933c18406ed263b62f7df073fd2e80301fc4814a
Ricky Caballero <cabaethernet@gmail.com>
2018-02-06 22:01:51 -0500

    |change|Local Host

--- public/chat.js
+++ public/chat.js
@@ -2,7 +2,7 @@ window.onload = function() {
  
 	var messages = [];
 	var connections = [];
-	var socket = io.connect('http://localhost:3700');
+	var socket = io.connect('http://localhost:5000');
 	var field = document.getElementById("field");
 	var sendButton = document.getElementById("send");
 	var content = document.getElementById("content");
